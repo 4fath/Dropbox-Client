7a92583d462125cd810a4d7646763f760cebbf05
fatih <fatihsenturk@mail.com>
2017-10-31 23:19:40 +0300

    dummy/wrong test cases deleted

--- src/test/java/org/fath/AppTest.java
+++ src/test/java/org/fath/AppTest.java
@@ -1,29 +1,13 @@
 package org.fath;
 
-import com.dropbox.core.DbxException;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-
-import java.io.IOException;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest extends TestCase {
-
-
-    private static String APP_KEY = "i353qnxe0gx5odm";
-    private static String APP_SECRET = "b1brlhfpm1n7gji";
-
-    private static String AUTH_COMMAND = "auth";
-    private static String INFO_COMMAND = "info";
-    private static String LIST_COMMAND = "list";
-
-    public void tp() throws IOException, DbxException {
-        App.main(new String[]{"fsdf", "sdfs"});
-        assertTrue(true);
-    }
 
     /**
      * Create the test case
@@ -39,33 +23,5 @@ public class AppTest extends TestCase {
      */
     public static Test suite() {
         return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testAuth() throws IOException, DbxException {
-        App.main(new String[]{AUTH_COMMAND, APP_KEY, APP_SECRET});
-        assertTrue(true);
-    }
-
-
-    public void testInfo() throws IOException, DbxException {
-        //App.main(new String[]{INFO_COMMAND, "sdfs"});
-        String fileName = "loa.pdf";
-
-        int indexof = fileName.lastIndexOf(".");
-        String mimiType = fileName.substring(indexof, fileName.length());
-    }
-
-    public void testLıst() throws IOException, DbxException {
-
-
-
-        long value =  123456789;
-        float val = (float) value / (1024*1024);
-        String formattedValue = String.format("%.2f", val);
-
-        assertTrue(true);
     }
 }
